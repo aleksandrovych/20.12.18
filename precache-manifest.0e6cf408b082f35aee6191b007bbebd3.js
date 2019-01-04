398094389a3b33f5ea777befa25e8246d20cb59a
Allar-Alexey Alexandrovich <allar5430@gmail.com>
2019-01-04 11:10:51 +0200

    add build artifacts

---
+++ precache-manifest.0e6cf408b082f35aee6191b007bbebd3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/icons.af7ae505.woff2"
+  },
+  {
+    "revision": "9720ad98c44e4e670d6d",
+    "url": "/static/css/main.553d61e8.chunk.css"
+  },
+  {
+    "revision": "274ce903d83e35377eeb",
+    "url": "/static/js/1.274ce903.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "9720ad98c44e4e670d6d",
+    "url": "/static/js/main.9720ad98.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/icons.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/icons.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/icons.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/icons.912ec66d.svg"
+  },
+  {
+    "revision": "580c0f5eac7a07b4bb184acd5afc5141",
+    "url": "/index.html"
+  }
+];
