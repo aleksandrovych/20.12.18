0bb31cdef5fccf4b56a79db398c432f4b5751796
Allar-Alexey Alexandrovich <allar5430@gmail.com>
2019-01-04 11:51:21 +0200

    remove some folders

--- moveirstore/build/service-worker.js
+++ moveirstore/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e6cf408b082f35aee6191b007bbebd3.js"
+  "/20.12.18/precache-manifest.723d04bdf578734d237f83eefe74d15f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/20.12.18/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
